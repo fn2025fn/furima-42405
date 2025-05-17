328ff57f707748a9e372acc4347d4380eb700fd0
fn2025fn <usui.fn.2024@gmail.com>
2025-05-17 16:23:22 +0900

    ルーティング、バリデーション仮

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price,
+            presence: true
 end
