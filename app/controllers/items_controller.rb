56014b9b919e553bec25d396890646739096cacb
fn2025fn <usui.fn.2024@gmail.com>
2025-05-17 16:46:35 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
   validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price,
             presence: true
 end
