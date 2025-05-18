97f6f5d7254f1ed36d376c61540ada37013acd30
fn2025fn <usui.fn.2024@gmail.com>
2025-05-19 02:50:43 +0900

    商品情報がデータベースに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :shipping_free
+  belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_time
 
