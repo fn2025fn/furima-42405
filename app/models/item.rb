fcd200af18a43e87c99aee60eb7ebaccd399d035
fn2025fn <usui.fn.2024@gmail.com>
2025-05-18 23:38:27 +0900

    ActiveHashを導入、商品出品ページを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_free
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
   validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price,
             presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
