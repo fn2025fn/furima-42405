846337b88454708914f8eee8da0e892a8027c5d6
fn2025fn <usui.fn.2024@gmail.com>
2025-05-19 12:06:02 +0900

    商品出品ページへの遷移,エラーハンドリングの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,6 +23,6 @@ class Item < ApplicationRecord
                       only_integer: true,
                       greater_than_or_equal_to: 300,
                       less_than_or_equal_to: 9_999_999,
-                      message: 'は300円以上9,999,999円以下の半角数字で入力してください'
+                      message: 'is not a number'
                     }
 end
