d87e19acca95348326a43281846e448e2a421ca6
fn2025fn <usui.fn.2024@gmail.com>
2025-05-19 16:51:21 +0900

    単体テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -66,5 +66,10 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not a number')
     end
+    it 'userが紐づいていないと保存できないと保存でいない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
   end
 end
