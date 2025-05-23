d3baedfec3a219bde5075017340f39d75d46f87f
fn2025fn <usui.fn.2024@gmail.com>
2025-05-23 15:54:25 +0900

    Formオブジェクトを作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,30 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone_number
+    validates :token
+  end
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message:  'must be 10 or 11 digits without hyphens' }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+
+    Address.create(postal_code: postal_code,
+                   prefecture_id: prefecture_id,
+                   city: city,
+                   address: address,
+                   building: building,
+                   phone_number: phone_number,
+                   order_id: order.id)
+  end
+end
