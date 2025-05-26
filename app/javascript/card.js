8d32f29dee075cb7357a3cc546e70a8847a09eab
fn2025fn <usui.fn.2024@gmail.com>
2025-05-26 14:48:18 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -10,6 +10,7 @@ const pay = () => {
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
   const form = document.getElementById('charge-form')
+  if(!form)return;
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
